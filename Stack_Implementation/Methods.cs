efa7852cc74bdbbed7c9aa5e813928d89e9ba3dc
Yogi120 <me.yogesh.pal@gmail.com>
2023-06-26 23:13:36 +0530

    Push Method Implementation

---
+++ Stack_Implementation/Methods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Implementation
+{
+    public class Stack
+    {
+        private class Node
+        {
+            public int data;
+            public Node next;
+
+            public Node(int data)
+            {
+                this.data = data;
+                next = null;
+            }
+        }
+
+        static Node head;
+        public bool isEmpty()
+        {
+            return head == null;
+        }
+
+        public void push(int data)
+        {
+            Node newNode = new Node(data);
+            if (isEmpty())
+            {
+                head = newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
+        } 
+    }
+}
---
+++ Stack_Implementation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Implementation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stack s = new Stack();
+
+            s.push(1);
+            s.push(7);
+            s.push(4);
+            s.push(9);
+            s.push(5);
+
+          
+        }
+    }
+}
